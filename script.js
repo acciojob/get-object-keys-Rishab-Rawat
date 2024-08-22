2e3f61c436298b0222650e2e298848d234f8104f
Rishab-Rawat <rawatrishab66@gmail.com>
2024-08-22 18:00:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+Get Object Keys
+
+
+const student = {
+    name: "Rishab"
+};
+
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
+};
+
+console.log(student.getKeys());
